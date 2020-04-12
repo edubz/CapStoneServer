3728bd3c6138a4b2b6404aa3e12c3db4fdb21c3c
xmeatballx <erik.jensen5@pcc.edu>
2020-04-12 15:53:11 -0700

    server implemented according to mongodb atlas documentation but cannot establish connection

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
-
-
 app.use(express.static('public'));
 
 
-app.listen(port, function(){
+var http = require('http').Server(app);
+var port = process.env.PORT || 3000;
+
+var io = require('socket.io')(http);
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://user:capstone@pcc-creative-coding-capstone-3cnck.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect((err, client) => {
+  if (err) console.log('failed to connect')
+  else {
+    console.log('connected')
+  }
+});
+
+  app.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
+
+
+
+
+
+
+
+
+
+
