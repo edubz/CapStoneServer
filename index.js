a08a8a5dc6e618a0f691a9b25ac5cd5963511afe
xmeatballx <erik.jensen5@pcc.edu>
2020-04-12 17:06:56 -0700

    added template for client-side slider control in sketch.js

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 var app = express();
+var server = require('http').Server(app);
 app.use(express.static('public'));
 
+server.listen(3000);
 
 var http = require('http').Server(app);
 var port = process.env.PORT || 3000;
 
-var io = require('socket.io')(http);
+var io = require('socket.io')(server);
+io.on('connection', function (socket) {
+	console.log('new connection at ' + socket.id);
+  	socket.on('report', function (val) {
+    console.log(socket.id + ':' + val);
+  });
+});
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://user:capstone@pcc-creative-coding-capstone-3cnck.mongodb.net/test?retryWrites=true&w=majority";
@@ -14,13 +22,13 @@ const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect((err, client) => {
   if (err) console.log('failed to connect')
   else {
-    console.log('connected')
+    console.log('connected');
   }
 });
 
-  app.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+//   app.listen(port, function(){
+//   console.log('listening on *:' + port);
+// });
 
 
 
--- public/sketch.js
+++ public/sketch.js
@@ -1,1 +1,13 @@
 var socket=io.connect('http://localhost:3000');
+
+
+function setup(){
+		var slider= createSlider();
+		slider.input(reportValue);
+}
+
+function reportValue(){  
+	var val = this.value();
+    socket.emit('report', val);
+
+}
