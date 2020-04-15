50328a6b0b18a4fbf2c8c2c5108e6432cbaa99e5
xmeatballx <erik.jensen5@pcc.edu>
2020-04-14 17:16:22 -0700

    added osc send/receive basic framework

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//declare express and start server instance
 const express = require('express');
 var app = express();
 var server = require('http').Server(app);
@@ -8,6 +9,7 @@ server.listen(3000);
 var http = require('http').Server(app);
 var port = process.env.PORT || 3000;
 
+//declare and start socket.io
 var io = require('socket.io')(server);
 io.on('connection', function (socket) {
 	console.log('new connection at ' + socket.id);
@@ -16,15 +18,72 @@ io.on('connection', function (socket) {
   });
 });
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://user:capstone@pcc-creative-coding-capstone-3cnck.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect((err, client) => {
-  if (err) console.log('failed to connect')
-  else {
-    console.log('connected');
-  }
+
+//declare and start osc UDP
+var osc = require("osc");
+
+var getIPAddresses = function () {
+    var os = require("os"),
+        interfaces = os.networkInterfaces(),
+        ipAddresses = [];
+
+    for (var deviceName in interfaces) {
+        var addresses = interfaces[deviceName];
+        for (var i = 0; i < addresses.length; i++) {
+            var addressInfo = addresses[i];
+            if (addressInfo.family === "IPv4" && !addressInfo.internal) {
+                ipAddresses.push(addressInfo.address);
+            }
+        }
+    }
+
+    return ipAddresses;
+};
+
+var udpPort = new osc.UDPPort({
+    localAddress: "0.0.0.0",
+    localPort: 57121
 });
+
+udpPort.on("ready", function () {
+    var ipAddresses = getIPAddresses();
+
+    console.log("Listening for OSC over UDP.");
+    ipAddresses.forEach(function (address) {
+        console.log(" Host:", address + ", Port:", udpPort.options.localPort);
+    });
+    // For most Ports, send() should only be called after the "ready" event fires.
+    this.send({
+        address: "/carrier/frequency",
+        args: [
+            {
+                type: "f",
+                value: 0
+            }
+        ]
+
+	});
+});
+
+udpPort.on("message", function (oscMessage) {
+    console.log(oscMessage);
+});
+
+udpPort.on("error", function (err) {
+    console.log(err);
+});
+
+udpPort.open();
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://user:capstone@pcc-creative-coding-capstone-3cnck.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect((err, client) => {
+//   if (err) console.log('failed to connect')
+//   else {
+//     console.log('connected');
+//   }
+// });
 
 //   app.listen(port, function(){
 //   console.log('listening on *:' + port);
