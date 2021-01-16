a407b267600981ac47ad2847d57ba859dffa3a77
jmej <jmejia@anestheticaudio.com>
2021-01-15 16:28:14 -0800

    working esp32 code

--- esp32/OSC-sender/OSC-sender.ino
+++ esp32/OSC-sender/OSC-sender.ino
@@ -2,12 +2,13 @@
 #include <WiFiUdp.h>  
 #include <OSCMessage.h>
 
-const char * ssid = "wall_of_static";
-const char * password = "cyfermastertm";
+const char * ssid = "wifi_ssid";
+const char * password = "wifi-pass";
 
+int sensorValue = 0;
+int sensorPin = 36; //
 
-
-IPAddress outIp(192, 168, 1, 140);
+IPAddress outIp(98, 246, 240, 250); //public ip of the computer running max
 const unsigned int outPort = 9999;
 WiFiUDP Udp;
 
@@ -16,21 +17,30 @@ void setup(){
     WiFi.mode(WIFI_STA);
     Udp.begin(outPort);
     WiFi.begin(ssid, password);
-    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-        Serial.println("WiFi Failed");
-        while(1) {
-            delay(1000);
-        }
+    
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
     }
+    
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
 void loop(){
 
+    sensorValue = analogRead(sensorPin);
+    
+//    Serial.print("sensor = ");
+//    Serial.print(sensorValue);
     OSCMessage msg("/sensor");
-    msg.add(666);
+    msg.add(sensorValue);
     Udp.beginPacket(outIp, outPort);
     msg.send(Udp); // send the bytes to the SLIP stream
     Udp.endPacket(); // mark the end of the OSC Packet
     msg.empty(); // free space occupied by message
+    delay(2);
 
 }
