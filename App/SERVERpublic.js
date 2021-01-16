0ed105c0851f5594e9980ed87a47f31eb5df37ce
xmeatballx <erik.jensen5@pcc.edu>
2021-01-15 17:25:00 -0800

    added getaudiofiles patch

--- App/SERVERpublic.js
+++ App/SERVERpublic.js
@@ -8,13 +8,17 @@ bucket using multer middleware.
 //declare express and start server instance
 const express = require("express");
 var app = express();
-var server = require("http").Server(app);
+var server = require("http").createServer(app);
 var port = process.env.PORT || 3000;
+const io = require('socket.io')(server);
+io.on('connection', (conn) => {
+  console.log('new conn')
+})
 
 //set for audio file upload from p5js
-const aws = require("aws-sdk");
-const multer = require("multer");
-const multer_s3 = require("multer-s3");
+// const aws = require("aws-sdk");
+// const multer = require("multer");
+ // const multer_s3 = require("multer-s3");
 const fs = require("fs");
 
 //gain access to s3
@@ -24,6 +28,7 @@ const fs = require("fs");
 //   region: process.env.region,
 // });
 
+const multer = require('multer');
 //configure the multer upload's storage settings. in this case uploads are stored in our s3 bucket
 const storage = multer.diskStorage({
   destination: function(req, file, cb){
@@ -38,15 +43,24 @@ const storage = multer.diskStorage({
 const upload = multer({storage: storage});
 
 //single file upload
-var uploadSingle = upload.single("soundBlob");
+function uploadSingle(){
+
+  upload.single("soundBlob");
+}
 
 //POST request route to allow p5js request to reach server in order to upload to s3 via multer
-app.post("/upload", function (req, res) {
-  uploadSingle(req, res, (err) => {
+app.post("/upload", upload.single("soundBlob"), function (req, res) {
+  // res.on('finish', () => {
+    // console.log(req)
+    io.emit('file uploaded', req.file.originalname)
+  // })
+  // uploadSingle(req, res, (err) => {
     
-  });
+  // });
    //console.log(res);
 });
+
+
 
 //serve static site and listen on port 3000 or build platform port
 app.use(express.static("public"));
