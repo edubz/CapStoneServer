e7cc2d7b4c3bbb8c64325783df9117a70ed06b0f
xmeatballx <erik.jensen5@pcc.edu>
2021-02-05 22:10:22 -0800

    added options back to udpPort creation

--- CapstoneServerRework/api/src/controllers/osc/createudpport.ts
+++ CapstoneServerRework/api/src/controllers/osc/createudpport.ts
@@ -1,5 +1,9 @@
 const osc = require("osc");
 
 export function createUdpPort(address: string, port: number) {
-    return new osc.UDPPort();
+    return new osc.UDPPort({
+        localAddress: address,
+        localPort: port,
+        metadata: true
+    })
 }
