c3c85087ed9c358562f947e670858569c76ac8d4
xmeatballx <erik.jensen5@pcc.edu>
2021-02-06 00:05:39 -0800

    changed local udp address to static ip of digitalocean droplet

--- CapstoneServerRework/api/src/server.ts
+++ CapstoneServerRework/api/src/server.ts
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 const webSocketServer = require("socket.io")(server);
 
 const testUdpPort = new osc.UDPPort({
-    localAddress: "127.0.0.1",
+    localAddress: "159.203.191.234",
     localPort: 57121,
     metadata: true
 })
