bb9cba63e512fd28487a71a23aa825cde2f2a559
xmeatballx <erik.jensen5@pcc.edu>
2021-02-05 20:57:25 -0800

    restored udpPort creation options to default

--- CapstoneServerRework/api/src/controllers/osc/createudpport.ts
+++ CapstoneServerRework/api/src/controllers/osc/createudpport.ts
@@ -2,8 +2,6 @@ const osc = require("osc");
 
 export function createUdpPort(address: string, port: number) {
     return new osc.UDPPort({
-        localAddress: address,
-        localPort: port,
         metadata: true
     })
 }
