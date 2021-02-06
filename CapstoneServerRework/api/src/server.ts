cf3e53e6dc0204ef61c2755c0ae8847592dd9731
xmeatballx <erik.jensen5@pcc.edu>
2021-02-06 00:13:22 -0800

    switched back to original udpPort after testing with test port

--- CapstoneServerRework/api/src/server.ts
+++ CapstoneServerRework/api/src/server.ts
@@ -12,20 +12,20 @@ const port = 5000;
 const server = http.createServer(app);
 const webSocketServer = require("socket.io")(server);
 
-const testUdpPort = new osc.UDPPort({
-    localAddress: "159.203.191.234",
-    localPort: 57121,
-    metadata: true
-})
+// const testUdpPort = new osc.UDPPort({
+//     localAddress: "159.203.191.234",
+//     localPort: 57121,
+//     metadata: true
+// })
 
 if (process.env.NODE_ENV != "test") {
     server.listen(port, () => {
         console.log(`app listening at port: ${port}`);
-        testUdpPort.open();
+        udpHostPort.open();
         createDbConnection(database, dbURI, dbOptions);
-        testUdpPort.on("ready", () => console.log("osc started"))
-        testUdpPort.on("error", (err: any) => console.log(err))
-        testUdpPort.on("message", (m: oscMessage) => console.log(m))
+        udpHostPort.on("ready", () => console.log("osc started"))
+        udpHostPort.on("error", (err: any) => console.log(err))
+        udpHostPort.on("message", (m: oscMessage) => console.log(m))
     })
 }
 
