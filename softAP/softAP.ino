78840b9415ca3d087e32a022c4e15dc5051a2b86
xmeatballx <erik.jensen5@pcc.edu>
2021-03-05 16:04:39 -0800

    replaced wifi connection reporting logic

--- softAP/softAP.ino
+++ softAP/softAP.ino
@@ -6,6 +6,7 @@ AsyncWebServer server(80);
 char *ssid = "myEsp";
 char *password = "testpassword";
 const char HTML[] PROGMEM = "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n    <meta charset=\"UTF-8\">\n    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n    <title>Wifi Credentials Input</title>\n</head>\n<body>\n    <form action=\"/\" method=\"POST\" >\n    <fieldset>\n        <label for=\"ssid\">Wifi Network Name</label>\n        <input type=\"text\" name=\"ssid\" id=\"ssid\" value=\"...\">\n        <label for=\"pw\">Wifi Password</label>\n        <input type=\"text\" name=\"pw\" id=\"pw\" value=\"...\">\n    </fieldset>\n    <fieldset>\n        <input type=\"submit\" value=\"submit\">\n    </fieldset>\n    </form>\n</body>\n</html>";
+int prevWifiStatus = 6;
 
 int sensorValue = 0;
 int sensorPin = 5; //
@@ -34,26 +35,25 @@ void setup() {
     Serial.println(request->getParam(0)->value());
     Serial.print(request->getParam(1)->name());
     Serial.println(":");
-    Serial.print(request->getParam(1)->value());
+    Serial.println(request->getParam(1)->value());
     const char *Wifissid = request->getParam(0)->value().c_str();
     const char *Wifipassword= request->getParam(1)->value().c_str();
     Udp.begin(outPort);
     WiFi.begin(Wifissid , Wifipassword );
     request->send(200, "text/html", HTML);
-//    while (WiFi.status() != WL_CONNECTED) {
-//        delay(500);
-//        Serial.print(".");
-//    }
-    
-    
   });
 }
 
+void reportWifiConnection() {
+  if (WiFi.status() != prevWifiStatus) Serial.println("connected to wifi");
+  prevWifiStatus = WiFi.status();
+}
+
 void loop(){
-    if (WiFi.status() == WL_CONNECTED) Serial.println("connected to Wifi");
+    reportWifiConnection();
     sensorValue = digitalRead(sensorPin);
     sensorValue++;
-    OSCMessage msg("/test_via_softap");
+    OSCMessage msg("/test_via_softap1");
     msg.add(sensorValue);
     Udp.beginPacket(outIp, outPort);
     msg.send(Udp); // send the bytes to the SLIP stream
