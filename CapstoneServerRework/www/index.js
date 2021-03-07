f0053bdeea7673ce4e439a1ed1b7f48509e32d33
Erik Jensen <erik@Eriks-Mac-mini.local>
2021-03-06 18:21:43 -0800

    added cors to react server

--- CapstoneServerRework/www/index.js
+++ CapstoneServerRework/www/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'build')));
 
