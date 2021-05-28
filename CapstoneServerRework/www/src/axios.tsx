08c134118af7364dfd1fc66ee2f10ed26583700a
Erik Jensen <erik@Eriks-Mac-mini.local>
2021-05-28 14:12:02 -0700

    changed axios instance url back to api url from localhost

--- CapstoneServerRework/www/src/axios.tsx
+++ CapstoneServerRework/www/src/axios.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    // baseURL: 'https://api.theinput.tk',
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://api.theinput.tk',
+    // baseURL: 'http://localhost:5000',
 });
